8733ad6ce648fe0995b4fcdee1fb0dfd4d06033a
isso0424 <isso04240129@gmail.com>
2020-07-03 03:34:09 +0900

    [Fix]gofmt書けてなくてすいませんでした...

--- main.go
+++ main.go
@@ -3,46 +3,46 @@ package main
 import (
 	"fmt"
 	"os"
-  "os/signal"
-  "syscall"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
 	session, err := discordgo.New()
-  if err != nil {
-    fmt.Println("Error in create session")
-    fmt.Println(err)
-  }
+	if err != nil {
+		fmt.Println("Error in create session")
+		fmt.Println(err)
+	}
 
 	session.Token = loadToken()
 
-  session.AddHandler(onMessageCreate)
+	session.AddHandler(onMessageCreate)
 
-  err = session.Open()
-  defer session.Close()
-  if err != nil {
-    fmt.Println(err)
-  }
+	err = session.Open()
+	defer session.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-  sc := make(chan os.Signal, 1)
-  signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 
-  fmt.Println("booted!!!")
+	fmt.Println("booted!!!")
 
-  <-sc
-  return
+	<-sc
+	return
 }
 
 func onMessageCreate(session *discordgo.Session, event *discordgo.MessageCreate) {
-  if event.Author.Bot {
-    return
-  }
-  /* メッセージを受け取った際の処理 */
+	if event.Author.Bot {
+		return
+	}
+	/* メッセージを受け取った際の処理 */
 }
 
 func loadToken() (token string) {
 	token = os.Getenv("DISCORD_TOKEN")
-  return
+	return
 }
